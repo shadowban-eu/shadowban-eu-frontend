f3a6bfa6f964e04a93df4376c8a0ed87e172bcf3
Raphael Beer <raphael.beer@protonmail.com>
2018-05-22 09:36:19 +0200

    Add: sync header text with screenName input value

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,5 +1,26 @@
 export default class UI {
   constructor() {
-    
+    this.screenName = document.getElementById('screenName');
+    this.screenNamePrefix = document.querySelector('.controls .input-field .prefix');
+    this.headerScreenName = document.querySelector('.header-screen_name');
+    this.screenName.addEventListener('keyup', this.updateHeaderScreenName);
   }
+  updateHeaderScreenName = () => {
+    const classes = this.screenNamePrefix.classList;
+    if (!this.screenName.value) {
+      classes.remove('invalid');
+      classes.remove('valid');
+      this.headerScreenName.innerText = '@username';
+      return;
+    }
+
+    if (!this.screenName.validity.patternMismatch) {
+      classes.remove('invalid');
+      classes.add('valid');
+    } else {
+      classes.remove('valid');
+      classes.add('invalid');
+    }
+    this.headerScreenName.innerText = `@${this.screenName.value}`;
+  };
 }
