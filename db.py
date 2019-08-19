4e645e1c9429195501c967af15a82937a1da2b29
Raphael Beer <raphael.beer@protonmail.com>
2019-08-19 14:47:22 +0200

    Fix: provoke and catch MongoDB connection timeout on startup...
      ... not with the first request, i.e. attempt to actually
          write to the DB

--- db.py
+++ db.py
@@ -1,15 +1,23 @@
 import copy
-from pymongo import MongoClient
+import traceback
+import sys
+from pymongo import MongoClient, errors as MongoErrors
 
 class Database:
     def __init__(self, host=None, port=27017, db='tester', collection_name='results'):
         try:
             print('[mongoDB] Connecting to ' + host + ':' + str(port))
             print('[mongoDB] Using Collection `' + collection_name + '` in Database `' + db + '`')
-            self.client = MongoClient(host, port)
+            self.client = MongoClient(host, port, serverSelectionTimeoutMS=3)
             self.db = self.client[db]
             self.results = self.db[collection_name]
-            print('[mongoDB] OK')
+
+            # test connection immediately, instead of
+            # trying to write in a request, later.
+            self.client.admin.command('ismaster')
+        except MongoErrors.ServerSelectionTimeoutError:
+            print(traceback.format_exc())
+            sys.exit('MongoDB connection timed out.')
         except:
             print(traceback.format_exc())
             sys.exit('MongoDB connection failed.')
