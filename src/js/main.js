35d553979f4c3bc2902d47f0b0839efc7defb348
root <root@shadowban-eu-fra-1.localdomain>
2019-08-23 21:16:39 +0000

    fix reply deboosting test error

--- src/js/main.js
+++ src/js/main.js
@@ -100,7 +100,7 @@ const fullTest = async (screenName) => {
     if (result.tests.more_replies.ban === false) {
       barrierResult = ['ok', 'No reply deboosting detected.'];
     } else if (result.tests.more_replies.ban === true) {
-      const offensive = result.tests.more_replies.stage > 0
+      const offensive = result.tests.more_replies.stage <= 0
         ? ''
         : ' Tweets are rated as potentially offensive.';
       barrierResult = ['ban', `Reply deboosting detected.${offensive}`];
