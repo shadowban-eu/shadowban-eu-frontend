23e28f454d0298af95a29613f69e5372284eca3c
Raphael Beer <raphael.beer@protonmail.com>
2019-08-24 23:55:42 +0200

    Fix: unknown transpiling target -> set 'browser: 2 last'

--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,16 @@ const config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    browsers: 'last 2 versions'
+                  }
+                }
+              ]
+            ],
             plugins: [
               [
                 '@babel/plugin-transform-runtime',
