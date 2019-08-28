de37ccff4e3251364d08bb565378e73ecfba7106
Raphael Beer <raphael.beer@protonmail.com>
2019-08-28 18:06:45 +0200

    Chore: remove obsolote qfdFaq update logics

--- src/js/ui/TechInfo.js
+++ src/js/ui/TechInfo.js
@@ -69,26 +69,8 @@ export default class TechInfo {
     });
   }
 
-  static updateQFD(results) {
-    document.querySelector('#qfdFAQ .techInfo').classList.remove('hide');
-    const contentElement = document.querySelector('#qfdFAQ .techContent');
-    if (results.QFD.isBanned !== undefined) {
-      const searchForLink = TechInfo.makeSearchLink(`from:@${results.canonicalName} filter:${results.QFD.method}s`);
-      const qfOffLink = TechInfo.makeSearchLink(results.QFD.query, false, 'turned off');
-      const qfOnLink = TechInfo.makeSearchLink(results.QFD.query, true, 'turned on');
-      contentElement.innerHTML = `We found <a href="https://twitter.com/${results.canonicalName}/status/${results.QFD.tweetId}">this
-        reference tweet</a> by searching for ${searchForLink} and
-        extracted the shortlink <a href="${results.QFD.query}">${results.QFD.query}</a> from this tweet. We found this
-        tweet with the quality filter ${qfOffLink} ${results.QFD.isBanned ? 'but we did not find' : 'and we found'}
-        it with the quality filter ${qfOnLink}.`;
-      if (results.QFD.login) {
-        contentElement.innerHTML += 'We did not find the reference tweet without being logged into Twitter.';
-      }
-    }
-  }
-
   static reset() {
-    document.querySelectorAll('#threadFAQ, #searchFAQ, #barrierFAQ, #qfdFAQ .techInfo')
+    document.querySelectorAll('#threadFAQ, #searchFAQ, #barrierFAQ .techInfo')
       .forEach(element => element.classList.add('hide'));
   }
 }
